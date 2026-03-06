795d8c93f137459a85659530c4b9834b5f032ac0
Thyrak12 <thyrak9167@gmail.com>
2026-03-06 14:32:34 +0700

    update the viewmodel

--- lib/ui/screens/library/view_model/library_view_model.dart
+++ lib/ui/screens/library/view_model/library_view_model.dart
@@ -9,13 +9,12 @@ class LibraryViewModel extends ChangeNotifier {
   List<Song> _songs = [];
   final PlayerState _playerState;
 
-  LibraryViewModel(this._repository, this._playerState){
+  LibraryViewModel(this._repository, this._playerState) {
     init();
   }
 
   Future<void> init() async {
     _songs = _repository.fetchSongs();
-    _playerState.addListener(onPlayerStateChanged);
     notifyListeners();
   }
 
@@ -31,10 +30,12 @@ class LibraryViewModel extends ChangeNotifier {
 
   void play(Song song) {
     _playerState.start(song);
+    notifyListeners();
   }
 
   void stop() {
     _playerState.stop();
+    notifyListeners();
   }
 
   bool isPlaying(Song song) {
@@ -44,15 +45,11 @@ class LibraryViewModel extends ChangeNotifier {
   void tapSong(Song song) {
     if (_playerState.currentSong == song) {
       _playerState.stop();
+      notifyListeners();
     } else {
       _playerState.start(song);
+      notifyListeners();
     }
   }
-  
 
-  @override
-  void dispose() {
-    _playerState.removeListener(onPlayerStateChanged);
-    super.dispose();
-  }
 }
